3429f14d340703eb9893f49ecb2ea9ec1c7b6283
後藤　樹 <mgmgmg@ab.cyberhome.ne.jp>
2017-11-18 00:13:41 +0900

    ScriptTest
    配列を宣言して出力しました。

---
+++ Assets/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] points = new int[5];
+
+        // 配列の各要素に値を代入する
+        points[0] = 5;
+        points[1] = 15;
+        points[2] = 25;
+        points[3] = 35;
+        points[4] = 45;
+
+        // 配列の要素をすべて表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(points[i]);
+        }
+
+        for (int i = 4; i >= 0; i--) 
+        {
+            Debug.Log(points[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
